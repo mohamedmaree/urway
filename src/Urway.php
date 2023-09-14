60614f9963267c7682caf046eb397d07236e98a4
mohamed maree <m7mdmaree26@gmail.com>
2023-09-14 23:25:51 +0300

    Update Urway.php

--- src/Urway.php
+++ src/Urway.php
@@ -3,8 +3,8 @@ namespace maree\urway;
 use Carbon\Carbon;
 
 class Urway {
-    //$customerInfo = ['email' => 'customer@site.com']
-    public static function checkout($amount = 0.0 , $customerInfo = []) {
+    //$params= ['email' => 'customer@site.com','response_url' => route('your_callback_function')]
+    public static function checkout($amount = 0.0 , $params = []) {
             $random             = rand(1111111,9999999);
             $currency           = config('urway.currency');
             $merchantKey        = config('urway.merchantKey');
@@ -17,13 +17,18 @@ class Urway {
             $fields             = array(
                 'trackid'       => $random,
                 'terminalId'    => $name,
-                'customerEmail' => $customerInfo['email'],
+                'customerEmail' => $params['email'],
                 'action'        => "1", //visa , master , mada
                 'merchantIp'    => (getenv('SERVER_ADDR') == false)? $_SERVER['REMOTE_ADDR'] : getenv('SERVER_ADDR') ,
                 'password'      => $password,
                 'currency'      => $currency,
                 'country'       => config('urway.country'),
                 'amount'        => $amount,
+                "udf1"          => isset($params['udf1'])? $params['udf1'] : '',
+                "udf2"          => isset($params['response_url'])? $params['response_url'] : "https://urway.sa/urshop/scripts/response.php",//Response page URL
+                "udf3"          => isset($params['udf3'])? $params['udf3'] : '',
+                "udf4"          => isset($params['udf3'])? $params['udf4'] : '',
+                "udf5"          => isset($params['udf5'])? $params['udf5'] : '',
                 'requestHash'   => $hash
             );
             $data               = json_encode($fields);
@@ -65,7 +70,7 @@ class Urway {
     }
 
 
-    public static function checkoutStc($amount = 0.0 , $customerInfo = []){
+    public static function checkoutStc($amount = 0.0 , $params = []){
             $random             = rand(1111111,9999999);
             $currency           = config('urway.currency');
             $merchantKey        = config('urway.merchantKey');
@@ -78,7 +83,7 @@ class Urway {
             $fields             = array(
                 'trackid'       => $random,
                 'terminalId'    => $name,
-                'customerEmail' => $customerInfo['email'],
+                'customerEmail' => $params['email'],
                 'action'        => "13", //stc
                 'merchantIp'    => (getenv('SERVER_ADDR') == false)? $_SERVER['REMOTE_ADDR'] : getenv('SERVER_ADDR') ,
                 'password'      => $password,
